2458ab1d26555a215a163ea66d939fcc3cac6f98
Khamel Mumford <khamel.mumford@gmail.com>
2017-12-17 09:33:31 -0600

    JQueary conversion from office hours assistance code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,75 @@
 // Run at page load
+// Create an object that contains an array of objects with questions and answers
+var questions = [{
+        question: "Is the sky blue?",
+        answers: [
+            { answer: 'No', correct: false },
+            { answer: 'bad', correct: true },
+            { answer: 'yes', correct: true },
+        ]
+    },
+    {
+        question: "Is the sky blue2?",
+        answers: [
+            { answer: 'No2', correct: false },
+            { answer: 'bad2', correct: true },
+            { answer: 'yes2', correct: true },
+        ]
+    }
+];
+
+var currentQuestion = 0;
+console.log(currentQuestion);
+console.log(questions);
+
 window.onload = function() {
 
-    // Create list of question identifyers
-    var questions = {
-        question1: "What time is it?",
-        question2: "Where is the shoestore?",
-        question3: "How much do you love me?"
-    };
+    //Radio button on click alert this.index
+    // alert questions[0].answers[THIS.INDEX].correct
 
-    var answers = {
-        answer1: "Qui ora es!",
-        answer2: "donde estas la zeppateria!",
-        answer3: "Te quiero mucho con todo mi corazon!"
-    };
-    //radial button on.click events
-    function myFunction()
 
-    {
-        var rdiv1 = $("<div id='INPUT'>");
-        $("#INPUT").attr("type", "radio");
-        $("#INPUT").appendChild(rdiv1);
+    //answers[2].answer
+    function createQuestion() {
+
+        // var q = document.getElementById('question');
+        // var q = $("#question")
+
+        // var a = document.getElementById('answers');
+        // var a = $("#answers");
+
+        // q.innerHTML = "<span>" + questions[currentQuestion].question + "</span>";
+        $("#question").html("<span>" + questions[currentQuestion].question + "</span>");
+        for (var i = 0; i < questions[currentQuestion].answers.length; i++) {
+
+            // var answer =   document.createElement("p");
+            var answer = $("p");
+
+            // var radio = document.createElement("input");
+            var radio = $("input");
+
+            $("input").attr(type = "radio", name = "answer");
+            // radio.setAttribute("type", "radio");
+            // radio.setAttribute("name", "answer");
+            radio.attr("data-index", i);
+
+            // answer.innerHTML = questions[currentQuestion].answers[i].answer;
+            $("p").html(questions[currentQuestion].answers[i].answer);
+
+
+            radio.on("click", function() {
+                var index = this.attr("data-index");
+
+                alert(questions[currentQuestion].answers[index].correct);
+                alert(index);
+                currentQuestion++;
+                createQuestion();
+
+            })
+
+
+            $("p").append(radio);
+            $("#answers").append(answer);
+        }
     }
-    var userSelect = function() {
-        if (questions.question1 = answers.answer1) {
-            correct === correct++;
-        };
-    }
-}
-
-
-// Start timer prior to answer
-
-// Tally number of correct and wrong answers
-var correct = 0;
-var incorrect = 0;
+    createQuestion();
+};
