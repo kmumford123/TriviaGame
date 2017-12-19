5f7072424ffa9bf8a3a6fe0cc19d5444610b3756
Khamel Mumford <khamel.mumford@gmail.com>
2017-12-18 22:26:28 -0600

    Clock added, spans still overwriting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,18 +19,84 @@ var questions = [{
 ];
 
 var currentQuestion = 0;
-console.log(currentQuestion);
-console.log(questions);
 
 window.onload = function() {
+
+    //  Variable that will hold our setInterval that runs the stopwatch
+    var intervalId;
+
+    //prevents the clock from being sped up unnecessarily
+    var clockRunning = false;
+
+    // Our stopwatch object
+    var stopwatch = {
+
+        time: 120,
+        reset: function() {
+
+            stopwatch.time = 120;
+
+            // DONE: Change the "display" div to "00:00."
+            $("#display").text("02:00");
+
+        },
+        start: function() {
+
+            // DONE: Use setInterval to start the count here and set the clock to running.
+            if (!clockRunning) {
+                intervalId = setInterval(stopwatch.count, 1000);
+                clockRunning = true;
+            }
+        },
+        stop: function() {
+
+            // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+            clearInterval(intervalId);
+            clockRunning = false;
+        },
+
+        count: function() {
+
+            // DONE: increment time by 1, remember we cant use "this" here.
+            stopwatch.time--;
+
+            // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
+            //       and save the result in a variable.
+            var converted = stopwatch.timeConverter(stopwatch.time);
+            console.log(converted);
+
+            // DONE: Use the variable we just created to show the converted time in the "display" div.
+            $("#display").text(converted);
+        },
+        timeConverter: function(t) {
+
+            var minutes = Math.floor(t / 60);
+            var seconds = t - (minutes * 60);
+
+            if (seconds < 10) {
+                seconds = "0" + seconds;
+            }
+
+            if (minutes === 0) {
+                minutes = "00";
+            } else if (minutes < 10) {
+                minutes = "0" + minutes;
+            }
+
+            return minutes + ":" + seconds;
+        }
+    };
+    stopwatch.start();
 
     //Radio button on click alert this.index
     // alert questions[0].answers[THIS.INDEX].correct
     function listQuestions() {
-        var qvalue = $("<div>");
+
         for (var i = 0; i < questions.length; i++) {
-            $(qvalue).html("<span>" + questions[i].question + "</span>");
-            $("#questions").append(qvalue);
+            var qvalue = $("<span id=questions[i]>");
+            $(qvalue).html(`<span>  ${questions[i].question}  </span>`);
+            $("#question").html(qvalue);
+            console.log(questions[i].question)
         }
     }
     listQuestions();
@@ -76,6 +142,8 @@ window.onload = function() {
             $("#answers").append(radio);
             $("#answers").append(answer);
             console.log(answer);
+
+
         }
     }
     createQuestion();
